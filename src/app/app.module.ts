65ea2a7506b864ca9c7446d460e74829a32de6c9
nayakam <1308012+nayakam@users.noreply.github.com>
2019-09-03 06:57:14 +1000

    Container and child componet created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCatalogComponent } from './product-catalog/product-catalog.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCatalogComponent,
+    ShoppingCartComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
