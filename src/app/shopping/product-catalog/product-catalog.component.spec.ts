121bb908260eb55e63e2cfa8702b64c68110ac8d
nayakam <1308012+nayakam@users.noreply.github.com>
2019-09-04 22:30:12 +1000

    Add unit test to child components

--- src/app/shopping/product-catalog/product-catalog.component.spec.ts
+++ src/app/shopping/product-catalog/product-catalog.component.spec.ts
@@ -11,17 +11,29 @@ describe('ProductCatalogComponent', () => {
     TestBed.configureTestingModule({
       imports: [CoreModule],
       declarations: [ProductCatalogComponent]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductCatalogComponent);
     component = fixture.componentInstance;
+    component.productCatalog = [{id: 1, name: 'p1', price: 2.0},
+      {id: 2, name: 'p2', price: 2.5},
+      {id: 3, name: 'p3', price: 3.5}];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.productCatalog.length).toEqual(3);
+  });
+
+  it('Should validate', () => {
+    component.onProductSelectionChange(1);
+    component.productQuantity = 2;
+    component.onQuantitySelectionChange();
+    fixture.detectChanges();
+    expect(component.selectedProductPrice).toEqual(2.0);
+    expect(component.isAddValid).toEqual(true);
   });
 });
--- src/app/shopping/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/shopping/shopping-cart/shopping-cart.component.spec.ts
@@ -11,17 +11,37 @@ describe('ShoppingCartComponent', () => {
     TestBed.configureTestingModule({
       imports: [CoreModule],
       declarations: [ShoppingCartComponent]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ShoppingCartComponent);
     component = fixture.componentInstance;
+    component.cardItems = [
+      {product: {id: 1, name: 'p1', price: 2.0}, quantity: 2},
+      {product: {id: 2, name: 'p2', price: 2.5}, quantity: 2},
+      {product: {id: 3, name: 'p3', price: 3.5}, quantity: 3}
+    ];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should get total price', () => {
+    expect(component.cardItems.length).toEqual(3);
+    expect(component.getTotalValue()).toEqual(19.5);
+  });
+
+  xit('Should remove product from the card', () => {
+    expect(component.cardItems.length).toEqual(3);
+    const removeButton = fixture.debugElement.query((el) => {
+      return el.nativeElement.id === 'product-id-2';
+    });
+    removeButton.nativeElement.click();
+    expect(removeButton.nativeElement.textContent).toContain('X');
+    // TODO :::
+    // expect(component.cardItems.length).toEqual(2);
   });
 });
